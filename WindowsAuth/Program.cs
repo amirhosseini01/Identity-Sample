fcd9d35d59e34a6554171458e491f873e978552f
amir <amir.1derful@gmail.com>
2023-01-30 23:20:14 +0330

    windows authentication  last changes

---
+++ WindowsAuth/Program.cs
@@ -0,0 +1,49 @@
+using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate(options =>
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                options.EnableLdap(settings =>
+                {
+                    settings.Domain = "contoso.com";
+                    settings.MachineAccountName = "machineName";
+                    settings.MachineAccountPassword =
+                                      "134";
+                });
+            }
+        });
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
