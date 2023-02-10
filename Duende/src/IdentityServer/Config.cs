bc4e46c950c01ae02576410d9c5d628e6ed88add
amir <amir.1derful@gmail.com>
2023-02-10 20:49:39 +0330

    config the "Identity Server UI"

--- Duende/src/IdentityServer/Config.cs
+++ Duende/src/IdentityServer/Config.cs
@@ -1,14 +1,16 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        {
-            new IdentityResources.OpenId()
-        };
+     new List<IdentityResource>
+     {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+     };
 
     // Define API Scopedotnet add ./Api/Api.csproj package Microsoft.AspNetCore.Authentication.JwtBearer
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -19,23 +21,37 @@ public static class Config
 
     // Define Client
     public static IEnumerable<Client> Clients =>
-        new List<Client>
+     new List<Client>
+     {
+        // machine to machine client (from quickstart 1)
+        new Client
         {
-            new Client
+            ClientId = "client",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            // scopes that client has access to
+            AllowedScopes = { "api1" }
+        },
+        // interactive ASP.NET Core Web App
+        new Client
+        {
+            ClientId = "web",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
             {
-                ClientId = "client",
-
-                // no interactive user, use the clientid/secret for authentication
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-
-                // secret for authentication
-                ClientSecrets =
-                {
-                    new Secret("secret".Sha256())
-                },
-
-                // scopes that client has access to
-                AllowedScopes = { "api1" }
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
             }
-        };
+        }
+     };
 }
