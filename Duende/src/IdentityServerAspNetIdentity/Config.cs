0b2f2c221adfeb1f79f5f5f4e73e670b5e5be0cf
amir <amir.1derful@gmail.com>
2023-02-16 19:41:51 +0330

    using asp.net core identity as alternative

---
+++ Duende/src/IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,59 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope("api1", "My API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            // machine to machine client
+            new Client
+            {
+                ClientId = "client",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+                
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                    
+                // where to redirect to after login
+                RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+    
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
+            }
+        };
+}
